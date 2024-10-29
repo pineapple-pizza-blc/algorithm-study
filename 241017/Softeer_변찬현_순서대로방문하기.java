4be93308d28cb604eb583da947ea6ce7534370a8
chanhyun22 <bch0707@dongguk.edu>
2024-10-29 09:14:28 +0900

    Softeer_변찬현_순서대로방문하기

--- 241017/Softeer_변찬현_순서대로방문하기.java
+++ 241017/Softeer_변찬현_순서대로방문하기.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Softeer_순서대로방문하기 {
+public class Softeer_변찬현_순서대로방문하기 {
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 	static int[][] map;
