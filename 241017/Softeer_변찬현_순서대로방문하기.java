5b1b0287912c4de23df28d1243ad3a050fff0214
Chanhyun Byun <114146654+chanhyun22@users.noreply.github.com>
2024-10-29 09:16:44 +0900

    Byunchanhyun (#9)
    * 241017 문제풀이
    
    * 241017 문제풀이
    
    * Softeer_플레이페어암호
    
    * Softeer_로봇이지나간경로
    
    * Softeer_로봇이지나간경로
    
    * Softeer_교차로
    
    * Softeer_자동차테스트
    
    * Softeer_변찬현_안전을도와줄차세대지능형교통시슨템
    
    * Softeer_변찬현_자동차테스트
    
    * Softeer_변찬현_순서대로방문하기
    
    * Softeer_변찬현_플레이페어암호
    
    * Softeer_변찬현_로봇이지나간경로
    
    * Softeer_변찬현_교차로
    
    * Softeer_변찬현_안전을도와줄차세대지능형교통시스템

---
+++ 241017/Softeer_변찬현_순서대로방문하기.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Softeer_변찬현_순서대로방문하기 {
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int[][] map;
+	static int n, m, str, stc, edr, edc;
+	static int ans = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		map = new int[n][n];
+		int[][] visited = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int row = Integer.parseInt(st.nextToken()) - 1;
+			int col = Integer.parseInt(st.nextToken()) - 1;
+			visited[row][col] = i;
+			if (i == 0) {
+				str = row;
+				stc = col;
+				visited[str][stc] = -1;
+			}
+			if (i == m - 1) {
+				edr = row;
+				edc = col;
+			}
+		}
+
+		dfs(str, stc, 1, visited);
+		bw.write(ans + "");
+
+		br.close();
+		bw.close();
+	}
+
+	public static void dfs(int r, int c, int count, int[][] visited) {
+		if (r == edr && c == edc && count == m) {
+			ans++;
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if (nr >= 0 && nr < n && nc >= 0 && nc < n && map[nr][nc] == 0) {
+				if (visited[nr][nc] != -1) {
+					if (visited[nr][nc] != 0) {
+						if (count != visited[nr][nc]) {
+							continue;
+						}
+						int temp = visited[nr][nc];
+						visited[nr][nc] = -1;
+						dfs(nr, nc, count + 1, visited);
+						visited[nr][nc] = temp;
+					} else {
+						visited[nr][nc] = -1;
+						dfs(nr, nc, count, visited);
+						visited[nr][nc] = 0;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ 241029/Softeer_변찬현_안전을도와줄차세대지능형교통시스템.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.util.*;
+
+/*
+실행 시간
+265 ms
+메모리
+25.88 MB
+*/
+
+public class Softeer_변찬현_안전을도와줄차세대지능형교통시스템 {
+    static int N, T;
+    static int[][][] map;
+    static boolean[][] visited;
+    static int[][] moves = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+    static List<int[][]> signals = Arrays.asList(
+            new int[][]{{0, 1}, {0, 1}, {-1, 0}, {1, 0}},
+            new int[][]{{-1, 0}, {-1, 0}, {0, 1}, {0, -1}},
+            new int[][]{{0, -1}, {0, -1}, {-1, 0}, {1, 0}},
+            new int[][]{{1, 0}, {1, 0}, {0, 1}, {0, -1}},
+            new int[][]{{0, 1}, {0, 1}, {-1, 0}},
+            new int[][]{{-1, 0}, {-1, 0}, {0, -1}},
+            new int[][]{{0, -1}, {0, -1}, {1, 0}},
+            new int[][]{{1, 0}, {1, 0}, {0, 1}},
+            new int[][]{{0, 1}, {0, 1}, {1, 0}},
+            new int[][]{{-1, 0}, {-1, 0}, {0, 1}},
+            new int[][]{{0, -1}, {0, -1}, {-1, 0}},
+            new int[][]{{1, 0}, {1, 0}, {0, -1}}
+    );
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        map = new int[N][N][4];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < 4; k++) {
+                    map[i][j][k] = Integer.parseInt(st.nextToken()) - 1;
+                }
+            }
+        }
+
+        int answer = bfs();
+        bw.write(answer + "\n");
+
+        br.close();
+        bw.close();
+    }
+
+    static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1, 0});
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int dir = current[2];
+            int time = current[3];
+
+            if (!visited[x][y]) {
+                visited[x][y] = true;
+                count++;
+            }
+
+            if (time >= T || map[x][y][time % 4] % 4 != dir) continue;
+
+            if (isRange(x + moves[dir][0], y + moves[dir][1])) {
+                queue.add(new int[]{x + moves[dir][0], y + moves[dir][1], dir, time + 1});
+            }
+
+            if (map[x][y][time % 4] / 4 != 1 && isRange(x + moves[(dir + 3) % 4][0], y + moves[(dir + 3) % 4][1])) {
+                queue.add(new int[]{x + moves[(dir + 3) % 4][0], y + moves[(dir + 3) % 4][1], (dir + 3) % 4, time + 1});
+            }
+
+            if (map[x][y][time % 4] / 4 != 2 && isRange(x + moves[(dir + 1) % 4][0], y + moves[(dir + 1) % 4][1])) {
+                queue.add(new int[]{x + moves[(dir + 1) % 4][0], y + moves[(dir + 1) % 4][1], (dir + 1) % 4, time + 1});
+            }
+        }
+
+        return count;
+    }
+
+    static boolean isRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < N;
+    }
+}
