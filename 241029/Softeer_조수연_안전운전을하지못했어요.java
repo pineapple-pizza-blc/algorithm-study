fa10d32ea055a6ec6f42d69477762177dc3cc30c
조수연 <melthetensai@gmail.com>
2024-10-31 09:17:43 +0900

    Softeer_조수연_안전을도와줄차세대지능형교통시스템

--- 241029/Softeer_조수연_안전운전을하지못했어요.java
+++ 241029/Softeer_조수연_안전운전을하지못했어요.java
@@ -4,92 +4,111 @@ import java.io.IOException;
 import java.util.*;
 
 public class Softeer_조수연_안전운전을하지못했어요 {
-    static int n, t;
-    static boolean[][] check;
-    static int[][][] signals;
-    static Set<String> answer = new HashSet<>();
-    
-    static int[][] directions = {
-        {-1, 0}, {1, 0}, {0, -1}, {0, 1} // up, down, left, right
-    };
-    
-    static Map<Integer, int[][]> traffic = new HashMap<>() {{
-        put(1, new int[][] {{0, 1}, {-1, 0}, {0, 1}, {1, 0}});
-        put(2, new int[][] {{-1, 0}, {0, -1}, {-1, 0}, {0, 1}});
-        put(3, new int[][] {{0, -1}, {-1, 0}, {0, -1}, {1, 0}});
-        put(4, new int[][] {{1, 0}, {0, 1}, {1, 0}, {0, -1}});
-        put(5, new int[][] {{0, 1}, {-1, 0}, {0, 1}});
-        put(6, new int[][] {{-1, 0}, {0, -1}, {-1, 0}});
-        put(7, new int[][] {{0, -1}, {0, -1}, {1, 0}});
-        put(8, new int[][] {{1, 0}, {1, 0}, {0, 1}});
-        put(9, new int[][] {{0, 1}, {0, 1}, {1, 0}});
-        put(10, new int[][] {{-1, 0}, {-1, 0}, {0, 1}});
-        put(11, new int[][] {{0, -1}, {-1, 0}, {0, -1}});
-        put(12, new int[][] {{1, 0}, {0, -1}, {1, 0}});
-    }};
-    
+    static class Loc {
+        int x, y;
+
+        Loc(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    static HashSet<Integer> ans = new HashSet<>();
+
+    static int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1}, N, T, doro[][][];
+    static Loc[][] sign = new Loc[12][];
+    static int[][] dirIdx = new int[12][];
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        t = Integer.parseInt(input[1]);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        check = new boolean[n + 1][n + 1];
-        signals = new int[n + 1][n + 1][4];
+        N = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                String[] line = br.readLine().split(" ");
-                for (int k = 0; k < 4; k++) {
-                    signals[i][j][k] = Integer.parseInt(line[k]);
+        //(신호+1)%4 => 0 -> 하, 1 -> 우, 2 -> 상, 3 -> 좌
+
+        for(int i=0;i<4;i++) {
+            dirIdx[i] = new int[3];
+            for(int j=0;j<3;j++) {
+                dirIdx[i][j] = (2+i)-j;
+            }
+        }
+
+        //2 1 0, 3 2 1, 
+
+        doro = new int[N][N][4];
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<N;j++) {
+                st = new StringTokenizer(bf.readLine());
+                for(int k=0;k<4;k++) {
+                    doro[i][j][k] = Integer.parseInt(st.nextToken())-1;
                 }
             }
         }
 
         bfs();
-        System.out.println(answer.size());
+        System.out.println(ans.size());
     }
 
-    public static void bfs() {
-        Queue<State> q = new LinkedList<>();
-        q.offer(new State(1, 1, -1, 0, 0));
+    private static void bfs() {
+        Queue<State> queue = new LinkedList<>();
+        queue.offer(new State(0, 0, 0, 2));
         
-        while (!q.isEmpty()) {
-            State state = q.poll();
-            int x = state.x;
-            int y = state.y;
-            int px = state.px;
-            int py = state.py;
-            int time = state.time;
-            answer.add(x + "," + y);
-            int nowSignal = signals[x][y][time % 4];
-            int[][] moves = traffic.get(nowSignal);
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
 
-            if (px != moves[0][0] || py != moves[0][1]) continue;
+            int signal = doro[state.x][state.y][state.time];
+            // System.out.println(signal);
+            if((signal+1)%4 != state.dir) continue;
 
-            for (int i = 1; i < moves.length; i++) {
-                int nx = x + moves[i][0];
-                int ny = y + moves[i][1];
+            ans.add(state.x*100 + state.y);
+            
+            if(state.time == T) continue;
 
-                if (1 <= nx && nx <= n && 1 <= ny && ny <= n) {
-                    if (!check[nx][ny] && time < t) {
-                        check[nx][ny] = true;
-                        q.offer(new State(nx, ny, moves[i][0], moves[i][1], time + 1));
-                    }
-                }
-            }
+            
+
+            // if(signal < 4) {
+            //     //세 방향
+            //     for(int i=0;i<4;i++) {
+            //         int nextX = state.x+dx[i], nextY = state.y+dy[i];
+            //         if(isInRange(nextX, nextY) && i != (state.dir+2)%4) {
+            //             queue.add(new State(nextX, nextY, state.time+1, i));
+            //         }
+            //     }
+
+            // } else if(signal < 8) {
+            //     //dir 기준 dir - 1 빼고
+            //     for(int i=0;i<4;i++) {
+            //         int nextX = state.x+dx[i], nextY = state.y+dy[i];
+            //         if(isInRange(nextX, nextY) && i != (state.dir-1+4)%4 && i != (state.dir+2)%4) {
+            //             queue.add(new State(nextX, nextY, state.time+1, i));
+            //         }
+            //     }
+                
+            // } else {
+            //     for(int i=0;i<4;i++) {
+            //         int nextX = state.x+dx[i], nextY = state.y+dy[i];
+            //         if(isInRange(nextX, nextY) && i != (state.dir+1+4)%4 && i != (state.dir+2)%4) {
+            //             queue.add(new State(nextX, nextY, state.time+1, i));
+            //         }
+            //     }
+            // }
+
         }
     }
 
-    static class State {
-        int x, y, px, py, time;
-        
-        State(int x, int y, int px, int py, int time) {
-            this.x = x;
-            this.y = y;
-            this.px = px;
-            this.py = py;
+    static class State extends Loc{
+        State(int x, int y, int time, int dir) {
+            super(x, y);
             this.time = time;
+            this.dir = dir;
         }
+
+        int time, dir;
+
+    }
+
+    static boolean isInRange(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
     }
 }
